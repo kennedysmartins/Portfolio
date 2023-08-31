d17e8488de11c8fe63dc13632a5088088fa501a1
KillerDogs <kenybustamante2@hotmail.com>
2023-08-31 14:46:12 -0300

    Added new image changes

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 const imagemCelular1 = document.getElementById("fvec")
+const imagemCelular2 = document.getElementById("smartofficern")
+const imagemCelular3 = document.getElementById("portalmanaon")
 
-const caminhoDasImagens = [
+const caminhoDasImagens1 = [
   { src: "../src/phone-x-fvec.png" },
+  { src: "../src/phone-x-fvec2.png" },
+  { src: "../src/phone-x-fvec3.png" },
+]
+const caminhoDasImagens2 = [
   { src: "../src/phone-x-smartoffice.png" },
+  { src: "../src/phone-x-smartoffice2.png" },
+  { src: "../src/phone-x-smartoffice3.png" },
+]
+const caminhoDasImagens3 = [
   { src: "../src/phone-x-manaon.png" },
+  { src: "../src/phone-x-manaon2.png" },
+  { src: "../src/phone-x-manaon3.png" },
 ]
 
 let indiceBannerAtual = 0
 
 function atualizaBanner(indice) {
-  imagemCelular1.src = caminhoDasImagens[indice].src
+  imagemCelular1.src = caminhoDasImagens1[indice].src
+  imagemCelular2.src = caminhoDasImagens2[indice].src
+  imagemCelular3.src = caminhoDasImagens3[indice].src
 }
 
 function nextSlide() {
   if (imagemCelular1) {
-    if (indiceBannerAtual == caminhoDasImagens.length - 1) {
+    if (indiceBannerAtual == caminhoDasImagens1.length - 1) {
       indiceBannerAtual = 0
     } else {
       indiceBannerAtual = indiceBannerAtual + 1
@@ -28,15 +42,14 @@ let meuIntervalo = setInterval(nextSlide, 3000)
 async function carregaPosts() {
   const divPosts = document.getElementById("card")
   console.log(divPosts)
-  const response = await fetch("https://jsonplaceholder.typicode.com/posts/1/");
-  const postRecebido = await response.json();
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts/1/")
+  const postRecebido = await response.json()
 
   divPosts.innerHTML = `
   <br>
   <h5>${postRecebido.title}</h5>
   <p>${postRecebido.body}</p>
   `
-
 }
 
 carregaPosts()
@@ -44,13 +57,15 @@ carregaPosts()
 async function carregaComments() {
   const divComentarios = document.getElementById("comment")
   console.log(divComentarios)
-  const response = await fetch("https://jsonplaceholder.typicode.com/posts/1/comments")
-  const comentarioRecebido = await response.json();
+  const response = await fetch(
+    "https://jsonplaceholder.typicode.com/posts/1/comments"
+  )
+  const comentarioRecebido = await response.json()
 
-  let i = 1;
-  comentarioRecebido.forEach(Comment => {
-    if (i<=2) {
-      let divComment = document.createElement("div");
+  let i = 1
+  comentarioRecebido.forEach((Comment) => {
+    if (i <= 2) {
+      let divComment = document.createElement("div")
 
       divComment.innerHTML = `
       <br>
@@ -62,9 +77,7 @@ async function carregaComments() {
       divComentarios.appendChild(divComment)
       i++
     }
-  });
-
-
+  })
 }
 
 carregaComments()
